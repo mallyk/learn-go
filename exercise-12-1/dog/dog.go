8f57453d1406b9451b3fd9462dc2e5c2e47151a3
mallyk <kyle.mally@gmail.com>
2019-05-05 21:41:48 -0500

    documentation

---
+++ exercise-12-1/dog/dog.go
@@ -0,0 +1,7 @@
+//Package dog provides dog functionality
+package dog
+
+//Years converts human years into dog years
+func Years(humanYears int) int {
+	return humanYears * 7
+}
---
+++ exercise-12-1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mallyk/learn-go/exercise-12-1/dog"
+)
+
+func main() {
+	dogYears := dog.Years(8)
+	fmt.Println(dogYears)
+}
